231be595e3c5089796c0b658208776d9064f42d4
Glcn <gkisakol1@gmail.com>
2022-01-22 14:04:48 +0100

    1

--- src/api/auth-header.js
+++ src/api/auth-header.js
@@ -1,9 +1,1 @@
-const authHeader = () => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    return { Authorization: `Bearer ${token}` };
-  } else {
-    return {};
-  }
-};
-export default authHeader;
+
