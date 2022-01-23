4f88918822b160a772f5f6d694a27cc39753fdc2
gulcan <gkisakol1@gmail.com>
2022-01-23 22:15:20 +0100

    api1

--- src/api/auth-header.js
+++ src/api/auth-header.js
@@ -1,1 +1,11 @@
+const authHeader = () => {
+  const token = localStorage.getItem("token");
 
+  if (token) {
+    return { Authorization: `Bearer ${token}` };
+  } else {
+    return {};
+  }
+};
+
+export default authHeader;
