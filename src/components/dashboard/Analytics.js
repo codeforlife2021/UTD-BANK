620df9b5a9621992cb253f25a13a6bd5f0eab246
mehmetozbay <89272535+mehmetozbay@users.noreply.github.com>
2022-02-02 20:41:51 +0100

    v1

--- src/components/dashboard/Analytics.js
+++ src/components/dashboard/Analytics.js
@@ -1,7 +1,104 @@
 import React from "react";
+import styled from "styled-components";
+import { BsFillCalendar2WeekFill } from "react-icons/bs";
+import { IoStatsChart } from "react-icons/io5";
+import { BiGroup } from "react-icons/bi";
+import { FiActivity } from "react-icons/fi";
+import { cardStyles } from "./ReusableStyles";
+import { useStore } from "../../store";
+import SectionTitle from "../home/SectionTitle";
+export default function Analytics() {
+  const { userState, dispatchUser } = useStore();
+  const { user, isUserLogin } = userState;
+  return (
+    <Section>
+      <SectionTitle
+        title={`Hi ${user.firstName} ${user.lastName}`}
+        description="Welcome to UTDBANK"
+      />
+      <div className="analytics">
+        <div className="analytic ">
+          <div className="content">
+            <h5>Spent this month</h5>
+            <h2>$682.5</h2>
+          </div>
+          <div className="logo">
+            <BsFillCalendar2WeekFill />
+          </div>
+        </div>
+        <div className="analytic">
+          <div className="logo">
+            <IoStatsChart />
+          </div>
+          <div className="content">
+            <h5>Earnings</h5>
+            <h2>$350.40</h2>
+          </div>
+        </div>
+        <div className="analytic">
+          <div className="logo">
+            <BiGroup />
+          </div>
+          <div className="content">
+            <h5>New clients</h5>
+            <h2>321</h2>
+          </div>
+        </div>
+        <div className="analytic ">
+          <div className="content">
+            <h5>Activity</h5>
+            <h2>$540.50</h2>
+          </div>
+          <div className="logo">
+            <FiActivity />
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
+}
+const Section = styled.section`
+  .analytics {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1rem;
+    .analytic {
+      ${cardStyles};
+      padding: 1rem;
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      gap: 1rem;
+      transition: 0.5s ease-in-out;
+      &:hover {
+        background-color: #e63719;
+        color: #233765;
+        svg {
+          color: #233765;
+        }
+      }
+      .logo {
+        background-color: #f5f7fd;
+        border-radius: 3rem;
+        color: #e63719;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 1.5rem;
+        svg {
+          font-size: 1.5rem;
+        }
+      }
+    }
+  }
 
-const Analytics = () => {
-  return <div></div>;
-};
-
-export default Analytics;
+  @media screen and (min-width: 280px) and (max-width: 720px) {
+    grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+    .analytic {
+      &:nth-of-type(3),
+      &:nth-of-type(4) {
+        flex-direction: row-reverse;
+      }
+    }
+  }
+`;
--- src/pages/customer/DashboardPage.js
+++ src/pages/customer/DashboardPage.js
@@ -1,15 +1,67 @@
 import React from "react";
+import styled from "styled-components";
 import Footer from "../../components/common/Footer";
 import Navbar from "../../components/common/Navbar";
+import Spacer from "../../components/common/Spacer";
+import Analytics from "../../components/dashboard/Analytics";
 import DashboardNavbar from "./../../components/dashboard/DashboardNavbar";
+import Earnings from "./../../components/dashboard/Earnings";
+import Transfers from "./../../components/dashboard/Transfers";
+import Profile from "./../../components/dashboard/Profile";
+import FAQ from "./../../components/dashboard/FAQ";
 
 const DashboardPage = () => {
   return (
-    <>
-      <DashboardNavbar />
+    <Section>
+      <Spacer />
+      <div className="grid">
+        <div className="row__one">
+          <Analytics />
+          <FAQ />
+        </div>
+        <div className="row__two">
+          <Earnings />
+          <Transfers />
+          <Profile />
+        </div>
+      </div>
+      <Spacer />
       <Footer />
-    </>
+    </Section>
   );
 };
 
 export default DashboardPage;
+
+const Section = styled.section`
+  padding: 2rem;
+  height: 100%;
+  .grid {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    gap: 1rem;
+    margin-top: 2rem;
+    .row__one {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      height: 50%;
+      gap: 1rem;
+    }
+    .row__two {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      gap: 1rem;
+      height: 50%;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    margin-left: 0;
+    .grid {
+      .row__one,
+      .row__two {
+        grid-template-columns: 1fr;
+      }
+    }
+  }
+`;
